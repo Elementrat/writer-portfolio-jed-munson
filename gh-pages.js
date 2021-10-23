ff1f6e0c9db2b7902fd44635126a103690501488
Alex Sohail <elementrat@gmail.com>
2021-10-23 12:11:37 -0400

    update repo

--- gh-pages.js
+++ gh-pages.js
@@ -4,14 +4,14 @@ publish(
  'build', // path to public directory
  {
   branch: 'gh-pages',
-  repo: 'https://github.com/Elementrat/jed-munson-svelte.git', // Update to point to your repository
+  repo: 'https://github.com/Elementrat/writer-portfolio-jed-munson.git', // Update to point to your repository
   user: {
    name: 'Elementrat', // update to use your name
    email: 'elementrat@gmail.com' // Update to use your email
   },
   dotfiles: true
   },
-  () => {
-   console.log('Deploy Complete!');
+  (e) => {
+   console.log('Deploy Complete!', e);
   }
 );
