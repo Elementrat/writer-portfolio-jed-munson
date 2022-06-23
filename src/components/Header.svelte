51066596ddd9276cfb0de1580d92572458112fbd
Alex Sohail <elementrat@gmail.com>
2022-06-24 00:15:06 +0900

    meta head

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,10 +1,14 @@
-<style>
-    .header{
-        padding: 1em 1em;
-        grid-area: header;
-    }
-</style>
+<svelte:head>
+	<title>Jed Munson</title>
+</svelte:head>
 
-<div class = "header">
-    <h1 class="name">Jed Munson</h1>
+<div class="header">
+	<h1 class="name">Jed Munson</h1>
 </div>
+
+<style>
+	.header {
+		padding: 1em 1em;
+		grid-area: header;
+	}
+</style>
